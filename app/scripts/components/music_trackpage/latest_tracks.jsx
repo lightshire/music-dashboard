72fc8f4b30ad07bbf0e084728d691e4d917b7fbe
lyndeniece <lyndeniece@any.tv>
2015-02-19 11:39:17 +0800

    add trackpage

---
+++ app/scripts/components/music_trackpage/latest_tracks.jsx
@@ -0,0 +1,42 @@
+'use strict';
+var React = require('react/addons'),
+    _ = require('lodash'),
+    Tracks = require('./items/tracks'),
+    AlbumTracksStore = require('../../stores/album_tracks_stores'),
+    getStateFromStore = function() {
+        return {
+            albumtracks: AlbumTracksStore.getAll()
+        };
+    },
+    LatestTracks = React.createClass({
+    	getInitialState: function() {
+            return getStateFromStore();
+        },
+        componentDidMount: function() {
+            this.unsubscribe = AlbumTracksStore.listen(this._onChange);
+        },
+        componentWillUnmount: function() {
+            this.unsubscribe();
+        },
+        render: function() {
+        	var data = this.state.albumtracks,
+        	    items;
+
+        	items = _.map(data, function(item) {
+        	    return (<Tracks
+        	        id={item.id}
+        	        image={item.image}
+        	        title={item.title}
+        	        artist={item.artist} />);
+        	});
+            return (
+            	<div className='row'>
+            		{items}
+            	</div>
+            );
+        },
+        _onChange: function() {
+            this.setState(getStateFromStore());
+        }
+    });
+module.exports = LatestTracks;
---
+++ app/scripts/components/music_trackpage/most_downloaded.jsx
@@ -0,0 +1,12 @@
+'use strict';
+var React = require('react/addons'),
+    MostDownloaded = React.createClass({
+        render: function() {
+            return (
+                <div>
+                    Hello
+                </div>
+            );
+        }
+    });
+module.exports = MostDownloaded;
